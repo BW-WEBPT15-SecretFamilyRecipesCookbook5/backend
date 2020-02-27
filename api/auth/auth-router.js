4efc5cf498a501032796c403d16095267b623586
CodingCCarpenter <christineassists@aol.com>
2020-02-26 22:47:32 -0600

    add getById function and require users-model

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -3,4 +3,18 @@ const Users = require('../users/users-model.js');
 
 const router = express.Router();
 
+// GET api/users/:id
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    Users.getById(id)
+        .then((user) => {
+            res.status(200).json(user)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: 'Could not retrieve user using getById in auth-router.js'});
+        })
+})
+
 module.exports = router;
