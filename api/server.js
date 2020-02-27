b14892f36c1513a930044a155bae7b5e3524ac01
CodingCCarpenter <christineassists@aol.com>
2020-02-26 21:32:49 -0600

    complete basic server.js setup

--- api/server.js
+++ api/server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const authRouter = require('./auth/auth-router.js');
+const recipesRouter = require('./recipes/recipes-router.js');
+
+
 const server = express();
+
+server.use(express.json());
+server.use('/api/auth', authRouter);
+server.use('/api/recipes', recipesRouter);
 
 module.exports = server;
