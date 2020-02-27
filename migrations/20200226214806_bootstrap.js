d63c11dc3a1a4342adc4697177ca154e8dd2573c
CodingCCarpenter <christineassists@aol.com>
2020-02-26 22:23:59 -0600

    adding knex migration for users and recipes tables

--- migrations/20200226214806_bootstrap.js
+++ migrations/20200226214806_bootstrap.js
@@ -1,6 +1,41 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('users', tbl => {
+        tbl.increments('id')
+            .notNullable()
+            .unique();
+        tbl.string('username', 32)
+            .notNullable()
+            .unique();
+        tbl.string('password', 32)
+            .notNullable();
+        tbl.string('first-name', 32);
+        tbl.string('last-name', 32);
+        tbl.string('email', 32);
+    })
+    .createTable('recipes', tbl => {
+        tbl.increments('id')
+            .notNullable()
+            .unique();
+        tbl.string('title', 32)
+            .notNullable()
+            .unique();
+        tbl.string('recipe_image_src');
+        tbl.string('author', 32);
+        tbl.string('category_id')
+            .notNullable();
+        tbl.string('instructions', 32);
+        tbl.string('directions');
+        tbl.string('description');
+        tbl.string('user_id');
+        tbl.string('notes');
+    })
+    .createTable('category', tbl => {
+        tbl.increments('id')
+            .notNullable()
+            .unique();
+    })
 };
 
 exports.down = function(knex) {
